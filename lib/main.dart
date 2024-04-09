f6ffb9c9c7bbf637674b6fa749a83954832c850c
Your Name <you@example.com>
2024-04-09 14:47:51 -0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/Contractor/user_data_provider.dart';
+import 'package:flutter_application_1/consultans/user_data_provider_consultans.dart';
+import 'package:flutter_application_1/home/home_page.dart';
+import 'package:flutter_application_1/login/welcome_view.dart';
+import 'package:flutter_application_1/moreUsers/consultans/consultans_register.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => UserDataProviderContractor()),
+        ChangeNotifierProvider(create: (_) => UserDataProviderConsultant()),
+      ],
+      child: const  MaterialApp(
+        home: WelcomeView(),
+      ),
+    );
+  }
+}
+
+// 
